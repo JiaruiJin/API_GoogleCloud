6924b9060b90472ef8bdf344d3d1651950851432
JiaruiJin <43017807+JiaruiJin@users.noreply.github.com>
2018-09-20 12:29:17 -0400

    Update GoogleVision.py

--- GoogleVision.py
+++ GoogleVision.py
@@ -5,24 +5,31 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+def label():
 
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'resources/wakeupcat.jpg')
+    client = vision.ImageAnnotatorClient()
+    dir=path
+    num=0
+    for root,dirname,filenames in os.walk(dir):
+        for filename in filenames:
+            if os.path.splitext(filename)[1]=='.jpg':
+                num = num +1
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
 
-image = types.Image(content=content)
+    i=0
+    while (i<num):
+        file_name = os.path.join(os.path.dirname(__file__),path+'/'+str(i)+'.jpg')
+        with io.open(file_name, 'rb') as image_file:
+            content = image_file.read()
+        
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+        image = types.Image(content=content)
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
+        print('Labels:')
+        for label in labels:
+            print(label.description)
+        i += 1
+ 
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+label()
